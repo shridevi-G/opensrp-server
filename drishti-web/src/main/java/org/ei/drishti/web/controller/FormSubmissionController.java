e7163e2a10269a906539fa5ac46286ce10208907
rakesh.b <rakesh.b@dhanushinfotech.net>
2015-11-02 13:26:29 +0530

    Adding Multimedia file image/audio upload functionality

--- drishti-web/src/main/java/org/ei/drishti/web/controller/FormSubmissionController.java
+++ drishti-web/src/main/java/org/ei/drishti/web/controller/FormSubmissionController.java
@@ -3,15 +3,6 @@ package org.ei.drishti.web.controller;
 import static ch.lambdaj.collection.LambdaCollections.with;
 import static ch.lambdaj.collection.LambdaCollections.with;
 import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-import static ch.lambdaj.collection.LambdaCollections.with;
-
-import static ch.lambdaj.collection.LambdaCollections.with;
 import ch.lambdaj.function.convert.Converter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -23,13 +14,16 @@ import java.util.Iterator;
 import java.util.List;
 import org.ei.drishti.common.util.HttpAgent;
 import org.ei.drishti.common.util.HttpResponse;
+import org.ei.drishti.domain.Multimedia;
 import org.ei.drishti.dto.ANMVillagesDTO;
 import org.ei.drishti.dto.VillagesDTO;
 import org.ei.drishti.dto.form.FormSubmissionDTO;
+import org.ei.drishti.dto.form.MultimediaDTO;
 import org.ei.drishti.event.FormSubmissionEvent;
 import org.ei.drishti.form.domain.FormSubmission;
 import org.ei.drishti.form.service.FormSubmissionConverter;
 import org.ei.drishti.form.service.FormSubmissionService;
+import org.ei.drishti.service.MultimediaService;
 import org.ei.drishti.web.handler.FormsubmissionHandler;
 import org.json.JSONException;
 import org.motechproject.scheduler.gateway.OutboundEventGateway;
@@ -43,10 +37,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FormSubmissionController {
@@ -57,6 +53,9 @@ public class FormSubmissionController {
     private final String drishtiANMVillagesURL;
     private OutboundEventGateway gateway;
     private FormsubmissionHandler drishtiform;
+    
+    @Autowired
+    private MultimediaService multimediaService;
 
     @Autowired
     public FormSubmissionController(@Value("#{drishti['drishti.anm.village.url']}") String drishtiANMVillagesURL,
@@ -178,5 +177,36 @@ public class FormSubmissionController {
             return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(CREATED);
+    }
+    
+        @RequestMapping(headers = {"Accept=application/json"}, method = GET, value = "/multimedia-file")
+    @ResponseBody
+    public List<MultimediaDTO> getFiles(@RequestParam("anm-id") String providerId) {
+        logger.info("Get Multimedia file");
+
+        List<Multimedia> allMultimedias = multimediaService.getMultimediaFiles(providerId);
+
+        return with(allMultimedias).convert(new Converter<Multimedia, MultimediaDTO>() {
+            @Override
+            public MultimediaDTO convert(Multimedia md) {
+                return new MultimediaDTO(md.getCaseId(), md.getProviderId(), md.getContentType(), md.getFilePath(), md.getFileCategory());
+            }
+        });
+    }
+
+    @RequestMapping(headers = {"Accept=multipart/form-data"}, method = RequestMethod.POST, value = "/multimedia-file")
+    public ResponseEntity<String> uploadFiles(@RequestParam("anm-id") String providerId, @RequestParam("entity-id") String entityId, @RequestParam("content-type") String contentType, @RequestParam("file-category") String fileCategory, @RequestParam("file") MultipartFile file) {
+        logger.info("post request method");
+        MultimediaDTO multimediaDTO = new MultimediaDTO(entityId, providerId, contentType, null, fileCategory);
+
+        String status = multimediaService.saveMultimediaFile(multimediaDTO, file);
+        String[] status1=status.split(":");
+        
+        if((status1[0]).equalsIgnoreCase("success")){
+            return new ResponseEntity<>(new Gson().toJson(status1[1]), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(new Gson().toJson("fail"), HttpStatus.OK);
+        }
     }
 }
