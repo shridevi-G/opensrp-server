34b41d3a945a4a099d9c23c1aae792544a11706c
rakesh.b <rakesh.b@dhanushinfotech.net>
2015-12-01 14:34:31 +0530

    Update sql tables

--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_1__Initial_Setup.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_1__Initial_Setup.sql
@@ -43,3 +43,97 @@ CREATE TABLE report.service_provided (ID SERIAL, service_provider INTEGER NOT NU
   CONSTRAINT FK_SP_DI_ID FOREIGN KEY (indicator) REFERENCES report.dim_indicator (ID),
   CONSTRAINT FK_SP_DD_ID FOREIGN KEY (date_) REFERENCES report.dim_date (ID),
   CONSTRAINT FK_SP_DL_ID FOREIGN KEY (location) REFERENCES report.dim_location (ID));
+
+CREATE TABLE report.country_tb (id SERIAL,country_name character varying(100) NOT NULL,country_code character varying(10) NOT NULL,active boolean,
+CONSTRAINT country_tb_a PRIMARY KEY (id));
+
+CREATE TABLE report.county_tb (id SERIAL,country_name integer NOT NULL,county_name character varying(100),active boolean NOT NULL,
+CONSTRAINT county_tb_key UNIQUE (county_name),
+CONSTRAINT county_tb_pkey PRIMARY KEY (id),
+CONSTRAINT county_tb_fkey FOREIGN KEY (country_name) REFERENCES report.country_tb(id));
+
+CREATE TABLE report.district_new (id SERIAL,country_name integer NOT NULL,county_name integer NOT NULL,district_name character varying(100) NOT NULL,   active boolean NOT NULL,
+CONSTRAINT district_new_key UNIQUE (district_name),
+CONSTRAINT district_new_pkey PRIMARY KEY (id),
+CONSTRAINT district_new_fkey FOREIGN KEY (country_name) REFERENCES report.country_tb(id),
+CONSTRAINT district_new_fkey1 FOREIGN KEY (county_name) REFERENCES report.county_tb(id));
+
+CREATE TABLE report.subdistrict_new (id SERIAL,subdistrict character varying(100),active boolean NOT NULL,country integer NOT NULL,county integer NOT NULL,district integer NOT NULL,
+CONSTRAINT subdistrict_new_pkey PRIMARY KEY (id),
+CONSTRAINT subdistrict_new_key UNIQUE (subdistrict),
+CONSTRAINT subdistrict_new_fkey FOREIGN KEY (country) REFERENCES report.country_tb(id),
+CONSTRAINT subdistrict_new_fkey1 FOREIGN KEY (county) REFERENCES report.county_tb(id),
+CONSTRAINT subdistrict_new_fkey2 FOREIGN KEY (district) REFERENCES report.district_new(id));
+
+CREATE TABLE report.location_new (id SERIAL,location character varying(100),active boolean NOT NULL,country integer NOT NULL,county integer NOT NULL,district integer NOT NULL,subdistrict integer NOT NULL,
+CONSTRAINT location_new_pkey PRIMARY KEY (id),
+CONSTRAINT location_new_fkey1 FOREIGN KEY (country) REFERENCES report.country_tb(id),
+CONSTRAINT location_new_fkey2 FOREIGN KEY (county) REFERENCES report.county_tb(id),
+CONSTRAINT location_new_fkey3 FOREIGN KEY (district) REFERENCES report.district_new(id),
+CONSTRAINT location_new_fkey4 FOREIGN KEY (subdistrict) REFERENCES report.subdistrict_new(id));
+
+CREATE TABLE report.health_centers_new (id SERIAL,hospital_name character varying(200) DEFAULT NULL::character varying NOT NULL,hospital_type character varying(200) DEFAULT NULL::character varying NOT NULL,hospital_address character varying(200) NOT NULL,parent_hospital character varying(200) DEFAULT ' '::character varying,villages character varying(200),active boolean NOT NULL,country_name integer,county_name integer,district_name integer,subdistrict_name integer,
+CONSTRAINT health_centers_new_key UNIQUE (hospital_name),
+CONSTRAINT health_centers_new_key1 UNIQUE (id),
+CONSTRAINT health_centers_new_fkey1 FOREIGN KEY (country_name) REFERENCES report.country_tb(id),
+CONSTRAINT health_centers_new_fkey2 FOREIGN KEY (county_name) REFERENCES report.county_tb(id),
+CONSTRAINT health_centers_new_fkey3 FOREIGN KEY (district_name) REFERENCES report.district_new(id),
+CONSTRAINT health_centers_new_fkey4 FOREIGN KEY (subdistrict_name) REFERENCES report.subdistrict_new(id));
+
+CREATE TABLE report.user_masters_new (id SERIAL,user_role character varying(200) NOT NULL,user_id character varying(200) NOT NULL,name character varying(200) NOT NULL,password character varying(200) NOT NULL,confirm_password character varying(200) NOT NULL,phone_number character varying(20) NOT NULL,email character varying(200) NOT NULL,subcenter integer DEFAULT 0,villages character varying(200) NOT NULL,lastname character varying(200) NOT NULL,active boolean NOT NULL,hospital integer DEFAULT 0,county integer,district integer,subdistrict integer,country integer,
+CONSTRAINT user_masters_new_pkey PRIMARY KEY (id),
+CONSTRAINT user_masters_new_key UNIQUE (user_id),
+CONSTRAINT user_masters_new_fkey1 FOREIGN KEY (country) REFERENCES report.country_tb(id),
+CONSTRAINT user_masters_new_fkey2 FOREIGN KEY (county) REFERENCES report.county_tb(id),
+CONSTRAINT user_masters_new_fkey3 FOREIGN KEY (district) REFERENCES report.district_new(id),
+CONSTRAINT user_masters_new_fkey4 FOREIGN KEY (hospital) REFERENCES report.health_centers_new(id),
+CONSTRAINT user_masters_new_fkey5 FOREIGN KEY (subcenter) REFERENCES report.health_centers_new(id),
+CONSTRAINT user_masters_new_fkey6 FOREIGN KEY (subdistrict) REFERENCES report.subdistrict_new(id));
+
+CREATE TABLE report.app_configuration (id SERIAL,country_name integer NOT NULL,wife_age_min integer DEFAULT 0 NOT NULL,wife_age_max integer DEFAULT 0 NOT NULL,husband_age_min integer DEFAULT 0 NOT NULL,husband_age_max integer DEFAULT 0 NOT NULL,temperature_units character varying(20) NOT NULL,escalation_schedule integer NOT NULL,is_highrisk character varying(100),
+CONSTRAINT app_configuration_key UNIQUE (country_name),
+CONSTRAINT app_configuration_key UNIQUE (id),
+CONSTRAINT app_configuration_pkey PRIMARY KEY (id),
+CONSTRAINT app_configuration_fkey FOREIGN KEY (country_name) REFERENCES report.country_tb(id));
+
+CREATE TABLE report.directions (id SERIAL,directions character varying(200) NOT NULL,active boolean NOT NULL,
+CONSTRAINT directions_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.dosage (id SERIAL, dosage character varying(100) NOT NULL,active boolean NOT NULL,
+CONSTRAINT dosage_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.frequency (id SERIAL,active boolean NOT NULL,number_of_times character varying(100) NOT NULL,
+CONSTRAINT frequency_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.drug_info (id SERIAL,drug_name character varying(100) NOT NULL,frequency integer,dosage integer,direction integer,active boolean NOT NULL,anc_conditions character varying(200) DEFAULT NULL::character varying,pnc_conditions character varying(200) DEFAULT NULL::character varying,child_illness character varying(200) DEFAULT NULL::character varying,
+CONSTRAINT drug_info_key UNIQUE (drug_name),
+CONSTRAINT drug_info_pkey PRIMARY KEY (id),
+CONSTRAINT drug_info_fkey FOREIGN KEY (direction) REFERENCES report.directions(id),
+CONSTRAINT drug_info_fkey1 FOREIGN KEY (dosage) REFERENCES report.dosage(id),
+CONSTRAINT drug_info_fkey2 FOREIGN KEY (frequency) REFERENCES report.frequency(id));
+
+CREATE TABLE report.form_fields (id SERIAL,form_name character varying(50),field1 character varying(50),field2 character varying(50),field3 character varying(50),field4 character varying(50),field5 character varying(50),country integer,
+CONSTRAINT form_fields_pkey PRIMARY KEY (id),
+CONSTRAINT form_fields_fkey FOREIGN KEY (country) REFERENCES report.country_tb(id));
+
+CREATE TABLE report.anc_due (id SERIAL,entityid character varying(200) NOT NULL,patientnum character varying(200),anmnum character varying(200),visittype character varying(200),visitno integer,lmpdate character varying(200),womenname character varying(200),visitdate character varying(200),anmid character varying(200),
+CONSTRAINT anc_due_id_key UNIQUE (id),
+CONSTRAINT anc_due_pkey PRIMARY KEY (entityid));
+
+CREATE TABLE report.icd10 (id SERIAL, ICD10_Chapter character varying(200) NOT NULL, ICD10_Code character varying(100) NOT NULL,ICD10_Name character varying(100) NOT NULL,can_select boolean NOT NULL,status boolean NOT NULL,
+CONSTRAINT icd10_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.investigation (id SERIAL,service_group_name character varying(200) NOT NULL,investigation_name character varying(200) NOT NULL,is_active boolean NOT NULL,
+CONSTRAINT CK_SPT CHECK (service_group_name = 'procedures' OR service_group_name = 'radiology'OR service_group_name = 'laboratory'),
+CONSTRAINT investigations_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.poc_backup (id SERIAL,visitentityid character varying(100),entityidec character varying(100),anmid character varying(100),level character varying(35),clientversion character varying(35),serverversion character varying(35),visittype character varying(35),phc character varying(100),docid character(100),poc text,
+CONSTRAINT poc_backup_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.poc_table (id SERIAL,visitentityid character varying(100) NOT NULL,entityidec character varying(100) NOT NULL,anmid character varying(100) NOT NULL,level character varying(35) NOT NULL,clientversion character varying(35) NOT NULL,serverversion character varying(35) NOT NULL,visittype character varying(35) NOT NULL,phc character varying(100) NOT NULL,pending character varying(300),docid character varying(50) DEFAULT NULL::character varying,timestamp timestamp with time zone,wifename character varying(200),
+CONSTRAINT poc_table_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.visit_configuration (id SERIAL,anc_visit1_from_week integer,anc_visit1_to_week integer,anc_visit2_from_week integer,anc_visit2_to_week integer,anc_visit3_from_week integer,anc_visit3_to_week integer,anc_visit4_from_week integer,anc_visit4_to_week integer,
+CONSTRAINT visit_configuration_pkey PRIMARY KEY (id));
+
+CREATE TABLE report.ec_reg (id SERIAL,entityid character varying(200) NOT NULL,phonenumber character varying(200) NOT NULL);
