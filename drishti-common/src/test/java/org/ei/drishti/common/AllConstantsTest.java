f112686fbea4fc85ca7c3589d25379e5cd96f279
shridevi-G <sridevi.g@dhanushinfotech.net>
2015-12-08 18:07:15 +0530

    adding test classs for common module

---
+++ drishti-common/src/test/java/org/ei/drishti/common/AllConstantsTest.java
@@ -0,0 +1,20 @@
+package org.ei.drishti.common;
+
+import org.ei.drishti.common.monitor.Monitor;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AllConstantsTest {
+	private AllConstants allConstants;
+	 @Before
+	    public void setUp(){
+		 allConstants = new AllConstants();
+	    }
+
+	 
+	 @Test
+		public void test() throws Exception {
+
+			//new AllConstants("dfd","gff");
+		}
+}
