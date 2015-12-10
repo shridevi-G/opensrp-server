8f3c25cca059cc2e056d9605f0e55c0f817e7294
Rani-Biswas <rani.B@datamedu.com>
2015-12-10 15:38:22 +0530

    Adding Test case for Report Data Update

--- drishti-common/src/test/java/org/ei/drishti/common/domain/ReportDataUpdateRequestTest.java
+++ drishti-common/src/test/java/org/ei/drishti/common/domain/ReportDataUpdateRequestTest.java
@@ -1,5 +1,68 @@
 package org.ei.drishti.common.domain;
 
-public class ReportDataUpdateRequestTest {
+import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReportDataUpdateRequestTest {
+	
+	private ReportDataUpdateRequest reportDataUpdateRequest;
+
+	@Before
+	public void setUp(){
+		reportDataUpdateRequest = new ReportDataUpdateRequest();
+	}
+	
+	@Test
+	public void indicatorTest(){
+		reportDataUpdateRequest.indicator();		
+	}
+	
+	@Test
+	public void startDateTest(){
+		reportDataUpdateRequest.startDate();	
+	}
+	
+	@Test
+	public void endDateTest(){
+		reportDataUpdateRequest.endDate();	
+	}
+	
+	@Test
+	public void typeTest(){
+		reportDataUpdateRequest.type();
+	}
+	
+	@Test
+	public void reportingDataTest(){
+		reportDataUpdateRequest.reportingData();
+	}
+	
+	@Test
+	public void withIndicatorTest(){
+		reportDataUpdateRequest.withIndicator("indicator");
+	}
+	
+	@Test
+	public void withStartDateTest(){
+		reportDataUpdateRequest.withStartDate("startDate");
+	}
+	
+	@Test
+	public void withTypeTest(){
+		reportDataUpdateRequest.withType("type");
+	}
+	
+	@Test
+	public void withEndDateTest(){
+		reportDataUpdateRequest.withEndDate("endDate");
+	}
+	
+	@Test
+	public void withReportingDataTest(){
+		List<ReportingData> reportingData = null;
+		reportDataUpdateRequest.withReportingData(reportingData);
+	}
+	
 }
